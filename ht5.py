ac6131740c5e18a994f0079c82e425d2e01dd798
andreapaniagua <46696984+andreapaniagua@users.noreply.github.com>
2019-03-01 18:08:36 -0600

    Add files via upload

--- ht5.py
+++ ht5.py
@@ -4,6 +4,7 @@
 import simpy
 import random
 import math
+import statistics
 #Main en el que se hacen los procesos
 #referencia tsobre variables tomada de https://es.stackoverflow.com/questions/4034/cu%C3%A1l-es-la-diferencia-entre-usr-bin-python-y-usr-bin-env-python
 def procesos(env, componentes): #env permite correr programas modificados
@@ -14,10 +15,78 @@ def procesos(env, componentes): #env permite correr programas modificados
     for i in range(cantidadProcesos):
         tiempo = math.exp(1.0/intervaloProcesos)
         Proceso(i, env, componentes)
-        print (env.timeout(tiempo))  # tiempo en crearse
+        yield (env.timeout(tiempo))  # tiempo en crearse
 #Resultados requeridos
 #referencia https://docs.python.org/3/library/statistics.html
 desviacion = statistics.stdev(procesoTiempo) 
 tiempoProm = statistics.mean(procesoTiempo)         
  
 print (", La desviacion Estandar: ", desviacion, ","El tiempo promedio es:  ", tiempoProm, )
+
+#numero de instrucciones
+random.seed(10) #para generar siempre los random como se deses recurso: https://stackoverflow.com/questions/22639587/random-seed-what-does-it-do
+ram= 100
+cpu=1
+cantProcesos= 100
+intervaloProcesos=1
+instrucciones=6
+tiempoIO= 1
+tiempo= []
+
+class Componentes:
+       def __init__(self,env): #siempre en class se define init recurso:file:///C:/Users/Andrea%20Paniagua/Downloads/Python%20Crash%20Course.pdf%20(%20PDFDrive.com%20).pdf
+            self.RAM=simpy.Container(env,init=ram)
+            self.CPU= simpy.Resource(env,init=ram)
+
+class Proceso:
+       def __init__(self, id, env,componnetes):
+       self.env=env
+       self.instruccion=random.randint(1,19)
+       self.ramMin=random.randint(1,10)
+       self.id=id
+       self.terminated=False
+       self.componentes=componentes
+       self.primerTiempo=primerTiempo
+       self.finalTiempo=finslTiempo
+       self.todoTiempo=todoTiempo
+       self.proceso=rnv.Proceso(self.procesos(env,componentes))
+
+       def procesos(self,env,componentes):
+           self.primerTiempo=env.ya
+           print("El proceso: %s: Se ha creado en : %d" %/self.id, self.primerTiempo))
+with componentes.RAM.get(self.ramMin)as ram:
+    yield ram
+
+    #ram
+    print("El proceso es: %s: La ram es: %d (wait)"% (self.id, env.ya))
+    op=0
+
+    while not self.terminated:
+        with componentes.CPU.request() as request:
+            print("El proceso es: %s : La espera del CPU es de: %d (wait)"% (self.id, env.ya))
+            yield request
+
+            for i in range (instruccion):
+                if self.instruccion>  0:
+                    self.instruccion -=1
+                    op=random.randint(1,2)
+
+            yield env.timeout(1)
+
+            if op==1:
+                print("El proceso es de: %s: El In Out es %d"% (self.id, env.ya))
+                yield env.timeout(ioTime)
+
+            if self.instruccion==0:
+                self.terminated= True
+
+        print("El proceso es: %s : finalizo en: %d (Estado: Terminated)" %(self.id, env.ya))
+        componentes.RAM.put(self.minRam)
+
+self.finalTiempo= env.ya
+self.todoTiempo=int (self.finalTiempo - self.primerTiempo)
+procesoTiempo.insert(self.id, self.todoTiempo)
+
+    
+                    
+       
